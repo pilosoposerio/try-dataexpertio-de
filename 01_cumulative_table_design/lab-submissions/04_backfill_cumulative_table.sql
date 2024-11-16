e085123f4501a741b520f19ab3b485008320d340
Clinton Poserio <cposerio@appen.com>
2024-11-16 23:59:24 +0800

    Add queries for homework #1 tasks #1 and #2.

---
+++ 01_cumulative_table_design/lab-submissions/04_backfill_cumulative_table.sql
@@ -0,0 +1,97 @@
+-- dialect: postgresql
+-- this query will run a backfill
+-- to generate the cumulative data from 1970 to 2021
+
+do $$
+begin
+-- made a loop to programmatically 
+-- backfill all available seasons
+for i_year IN 1970..2021 loop
+insert into actors
+with last_year as (
+    select *
+    from
+        actors
+    where
+        current_year = (i_year - 1)
+),
+
+this_year as (
+    select
+        actorid,
+        actor,
+        year,
+        array_agg(
+            row(
+                film,
+                year,
+                votes,
+                rating,
+                filmid
+            )::film
+        )::film [] as films,
+        avg(rating) as average_rating,
+        count(*) as films_count
+    from
+        actor_films
+    where
+        year = i_year
+    group by
+        actorid,
+        actor,
+        year
+),
+
+cumulative_table as (
+    select
+        coalesce(
+            t.actorid,
+            l.actorid
+        ) as actorid,
+        coalesce(
+            t.actor,
+            l.actor
+        ) as actor,
+        case
+            -- when first time encounter
+            -- use the records from this year
+            when l.films is null then t.films
+            -- when not first encounter and there's new annual data
+            -- append new data to old data
+            when coalesce(
+                    t.films_count,
+                    0
+                ) > 0 then l.films || t.films
+                -- otherwise, just retain old data
+            else l.films
+        end::film [] as films,
+        coalesce(
+            t.year,
+            l.current_year + 1
+        ) as current_year,
+        case
+            when coalesce(t.films_count, 0) > 0
+                then (
+                    case
+                        when t.average_rating > 8 then 'star'
+                        when t.average_rating > 7 then 'good'
+                        when t.average_rating > 6 then 'average'
+                        else 'bad'
+                    end
+                )::quality_class
+            else l.quality_class
+        end as quality_class,
+        coalesce(t.films_count, 0) > 0 as is_active
+    from
+        last_year as l
+    full outer join this_year as t
+        on
+            l.actorid = t.actorid
+)
+
+select *
+from
+    cumulative_table;
+end loop;
+end;
+$$
