757594453d1140a8249b4f194c9661fc52a6795f
Clinton Poserio <cposerio@appen.com>
2024-11-17 18:52:00 +0800

    Create table for actors SCD.

---
+++ 01_cumulative_table_design/lab-submissions/06_ddl_actors_scd.sql
@@ -0,0 +1,10 @@
+create table actors_scd
+(
+    actorid text,
+    actor text,
+    is_active boolean,
+    quality_class quality_class,
+    record_hash text,
+    start_year integer,
+    end_year integer
+);
